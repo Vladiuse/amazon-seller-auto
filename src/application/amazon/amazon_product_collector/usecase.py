96414f236611c2d4a565ebe8d07c93db9fd368a0
vladiuse <vladiuse@gmail.com>
2024-09-12 16:14:51 +0300

    refactor usecase

--- src/application/amazon/amazon_product_collector/usecase.py
+++ src/application/amazon/amazon_product_collector/usecase.py
@@ -1,25 +1,25 @@
 import logging
 
-from src.adapters.amazon_product_collector import AmazonProductCollector
-from src.adapters.amazon_product_convertor import AmazonProductConvertor
 from src.application.amazon.amazon_product_collector.dto.product import AmazonProduct
+from src.application.amazon.amazon_product_collector.interfaces.product_collector import IAmazonProductCollector
 from src.application.amazon.dto import Asin, MarketplaceCountry
+from src.main.exceptions import MaxTriesError, ParserError
 
 logging.basicConfig(level=logging.INFO)
 
 
 class CollectAmazonProductsUseCase:
 
+    def __init__(self, product_collector: IAmazonProductCollector):
+        self._product_collector = product_collector
+
     def collect(self, items: list[tuple[Asin, MarketplaceCountry]]) -> list[AmazonProduct]:
-        collector = AmazonProductCollector(
-            product_convertor=AmazonProductConvertor(),
-        )
         products = []
         for asin, marketplace_country in items:
-            print(asin, marketplace_country)
             try:
-                product = collector.collect(asin=asin, marketplace_country=marketplace)
+                product = self._product_collector.collect(asin=asin, marketplace_country=marketplace_country)
+                logging.info(product)
                 products.append(product)
             except (ParserError, MaxTriesError) as e:
-                logging.error(e)
+                logging.error(e)  # TODO no error text
         return products
