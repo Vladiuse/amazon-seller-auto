8db45709c929dde6e4c89f1a6fb48f1ce9970228
vladiuse <vladiuse@gmail.com>
2024-09-12 17:34:51 +0300

    add clean and send data to table funcs

--- src/application/airtable_product_sender/interfaces/airtable_product_sender.py
+++ src/application/airtable_product_sender/interfaces/airtable_product_sender.py
@@ -1,45 +1,61 @@
-import requests as req
-
-from src.application.airtable_product_sender.dto.product_table import CREATE_AMAZON_PRODUCT_TABLE_DATA
-from src.application.amazon.amazon_report_product_collector import AmazonReportProduct
-from src.main.config import airtable_config
+import json
+import logging
 from pprint import pprint
+
+import requests as req
 from pyairtable import Api
+
+from src.application.airtable_product_sender.dto.product_table import (
+    CREATE_AMAZON_PRODUCT_TABLE_DATA,
+    AmazonProductTable,
+)
+from src.application.amazon.amazon_report_product_collector.dto.product import AmazonReportProduct
+from src.main.config import airtable_config
 
 airtable = Api(api_key=airtable_config.AIRTABLE_API_KEY)
 
+
 class AirTableProductSender:
 
-    def create_table(self):
+    def create_table(self) -> None:
         data = CREATE_AMAZON_PRODUCT_TABLE_DATA.model_dump_json(exclude_none=True)
-        print(data)
         BASE_ID = airtable_config.AIRTABLE_APP_ID
         headers = {
             'Authorization': f'Bearer {airtable_config.AIRTABLE_API_KEY}',
             'Content-Type': 'application/json',
         }
         url = f'https://api.airtable.com/v0/meta/bases/{BASE_ID}/tables'
+        req.post(url, headers=headers, data=data)
+
+    def send_products(self, table_id: str, products: list[AmazonReportProduct]):
+        data = [product.model_dump(exclude_none=True) for product in products]
+        table = airtable.table(base_id=airtable_config.AIRTABLE_APP_ID, table_name=table_id)
+        table.batch_create(data)
+
+        url = f'https://api.airtable.com/v0/{airtable_config.AIRTABLE_APP_ID}/{table_id}'
+        headers = {
+            'Authorization': f'Bearer {airtable_config.AIRTABLE_API_KEY}',
+            'Content-Type': 'application/json',
+        }
+        pprint(data)
+        data = json.dumps(data)
         res = req.post(url, headers=headers, data=data)
         print(res.json())
         print(res.status_code)
 
-    def send_products(self, table_id: str, products: list[AmazonReportProduct]):
-        data = [product.model_dump(exclude_none=True) for product in products]
+    def clean_table(self) -> None:
+        records = AmazonProductTable.all()
+        AmazonProductTable.batch_delete(records)
 
-
-        table = airtable.table(base_id=airtable_config.AIRTABLE_APP_ID,table_name=table_id)
-        pprint(table.batch_create(data))
-        # url = f'https://api.airtable.com/v0/{airtable_config.AIRTABLE_APP_ID}/{table_id}'
-        # print(url)
-        # headers = {
-        #     'Authorization': f'Bearer {airtable_config.AIRTABLE_API_KEY}',
-        #     'Content-Type': 'application/json',
-        # }
-        # pprint(data)
-        # data = json.dumps(data)
-        # res = req.post(url, headers=headers, data=data)
-        # print(res.json())
-        # print(res.status_code)
+    def send_products_to_table(self, products: list[AmazonReportProduct]) -> None:
+        products_to_send = []
+        for product in products:
+            data = product.model_dump(exclude_none=True)
+            data['marketplace_country'] = str(data['marketplace_country'])
+            product_to_send = AmazonProductTable(**data)
+            products_to_send.append(product_to_send)
+        res = AmazonProductTable.batch_save(products_to_send)
+        logging.info(res)
 
     def _prepare_request(self, products: list[AmazonReportProduct]) -> dict:
         records = []
