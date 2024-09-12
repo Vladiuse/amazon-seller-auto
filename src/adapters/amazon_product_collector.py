6ae664746fc0a009d434f6a4fdd350dc03c960ea
vladiuse <vladiuse@gmail.com>
2024-09-12 16:15:19 +0300

    add face request sender

--- src/adapters/amazon_product_collector.py
+++ src/adapters/amazon_product_collector.py
@@ -11,6 +11,7 @@ from src.application.amazon.amazon_product_collector.interfaces.product_collecto
 from src.application.amazon.dto import Asin, MarketplaceCountry
 from src.application.amazon.utils import retry
 from src.main.config import zenrows_conf
+from src.main.exceptions import MaxTriesError
 
 
 @dataclass
@@ -19,7 +20,6 @@ class AmazonProductCollector(IAmazonProductCollector):
 
     def collect(self, asin: Asin, marketplace_country: MarketplaceCountry) -> AmazonProduct:
         html = self.get_amazon_product_page(asin=asin, marketplace_country=marketplace_country)
-        self._write_html_in_file(asin=asin, marketplace_country=marketplace_country, html=html)
         return self.product_convertor.convert(html=html, asin=asin, marketplace_country=marketplace_country)
 
     @retry(
@@ -34,10 +34,25 @@ class AmazonProductCollector(IAmazonProductCollector):
         logging.info(url)
         logging.info('response status code: %s', res.status_code)
         res.raise_for_status()
-        return res.text
+        html = res.text
+        self._write_html_in_file(asin=asin, marketplace_country=marketplace_country, html=html)
+        return html
 
     def _write_html_in_file(self, asin: Asin, marketplace_country: MarketplaceCountry, html: str) -> None:
         file_name = f'{marketplace_country.country_code}_{asin.value}.html'
         file_path = os.path.join('media/zen_pars', file_name)
         with open(file_path, 'w') as file:
             file.write(html)
+
+
+
+class AmazonProductCollectorFromSavedFile(AmazonProductCollector):
+
+    def get_amazon_product_page(self, asin: Asin, marketplace_country: MarketplaceCountry) -> str:
+        files_dir = 'media/zen_pars'
+        file_name = f'{marketplace_country.country_code}_{asin.value}.html'
+        file_path = os.path.join(files_dir, file_name)
+        if os.path.exists(file_path):
+            with open(file_path) as file:
+                return file.read()
+        raise MaxTriesError
