f01059b7e6a1dbba0d9a7ce09055f85bb795683a
vladiuse <vladiuse@gmail.com>
2024-09-17 13:30:04 +0300

    refacrot exceptions

--- src/main/exceptions.py
+++ src/main/exceptions.py
@@ -1,16 +1,22 @@
-class ReportDocumentNotComplete(Exception):
+class ApplicationException(Exception):
+    """ApplicationException"""
+
+
+class ReportDocumentNotComplete(ApplicationException):
     """Trow if report document status not in []"""
 
 
-class ReportStatusError(Exception):
+class ReportStatusError(ApplicationException):
     """Throw if report processing status is FATAL or CANCELED"""
 
-class MaxTriesError(Exception):
+
+class MaxTriesError(ApplicationException):
     """Throw if @retry attempts exceed"""
 
-class ParserError(Exception):
+
+class ParserError(ApplicationException):
     """Throw if some data not found in html"""
+
 
 class HtmlElementNotFound(ParserError):
     """Throw if html element not found"""
-
