da60626aa9bd10f6cac75990b3e632e7e6fdb5bc
vladiuse <vladiuse@gmail.com>
2024-09-17 16:04:01 +0300

    refactor amazonReportProduct

--- src/application/amazon/amazon_report_product_collector/dto/product.py
+++ src/application/amazon/amazon_report_product_collector/dto/product.py
@@ -1,18 +1,17 @@
-from pydantic import BaseModel, ConfigDict, Field
+from dataclasses import dataclass
 
 from src.application.amazon.dto import AsinStr, MarketplaceCountry
 
 
-class AmazonReportProduct(BaseModel):
-    model_config = ConfigDict(populate_by_name=True)
-
+@dataclass
+class AmazonReportProduct:
     asin: AsinStr
-    name: str = Field(alias='product-name')
+    name: str
     marketplace_country: MarketplaceCountry
     sku: str
-    available: int = Field(alias='afn-fulfillable-quantity')
-    inbound: int = Field(alias='afn-inbound-shipped-quantity')
-    featured_offer: str = Field(alias='your-price')
-    inbound_receiving_qty: int = Field(alias='afn-inbound-receiving-quantity')
+    available: int
+    inbound: int
+    featured_offer: str
+    inbound_receiving_qty: int
     rating: float | None = None
     rating_reviews: int | None = None
