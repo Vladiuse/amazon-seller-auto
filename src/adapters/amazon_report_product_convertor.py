008b8e83cc5dd20460e8e6e44bdc7a246cdee0e4
vladiuse <vladiuse@gmail.com>
2024-09-17 16:04:21 +0300

    refactor amazonReportProductConverter

--- src/adapters/amazon_report_product_convertor.py
+++ src/adapters/amazon_report_product_convertor.py
@@ -8,12 +8,21 @@ from src.application.amazon.amazon_report_product_collector.interfaces.report_pr
 from src.application.amazon.dto import MarketplaceCountry
 
 
-class ReportProductConvertor(IReportProductConvertor):
+class ReportProductConverter(IReportProductConvertor):
 
     def convert(self, report_document_text: str, marketplace_country: MarketplaceCountry) -> list[AmazonReportProduct]:
         products = []
         reader = csv.DictReader(io.StringIO(report_document_text), delimiter='\t')
         for row in reader:
-            product = AmazonReportProduct(marketplace_country=marketplace_country, **row)
+            product = AmazonReportProduct(
+                asin=row['asin'],
+                name=row['product-name'],
+                marketplace_country=marketplace_country,
+                sku=row['sku'],
+                available=int(row['afn-fulfillable-quantity']),
+                inbound=int(row['afn-inbound-shipped-quantity']),
+                featured_offer=row['your-price'],
+                inbound_receiving_qty=int(row['afn-inbound-receiving-quantity']),
+            )
             products.append(product)
         return products
