c6b9dcd8f9740500cb7f03b612a970da8cc24b77
vladiuse <vladiuse@gmail.com>
2024-09-18 14:20:27 +0300

    add AmazonReport

--- src/adapters/amazon_report_creator.py
+++ src/adapters/amazon_report_creator.py
@@ -4,8 +4,13 @@ from sp_api.api import Reports as SpApiReports
 from sp_api.base import ReportType
 from sp_api.base.exceptions import SellingApiRequestThrottledException
 
-from src.application.amazon.amazon_report_product_collector.interfaces.amazon_report_creator import IAmazonReportCreator
+from src.application.amazon.amazon_report_product_collector.dto.report import AmazonReportProduct
+from src.application.amazon.amazon_report_product_collector.interfaces.amazon_report_creator import (
+    IAmazonReport,
+    IAmazonReportCreator,
+)
 from src.application.amazon.utils import retry
+from src.main.exceptions import ReportDocumentNotComplete, ReportStatusError
 
 
 class AmazonReportCreator(IAmazonReportCreator):
@@ -22,3 +27,25 @@ class AmazonReportCreator(IAmazonReportCreator):
         data = self.sp_api_reports.create_report(reportType=report_type)
         logging.info(data.payload)
         return data.payload['reportId']
+
+
+class AmazonReport(IAmazonReport):
+
+    def __init__(self, sp_api_reports: SpApiReports):
+        self.sp_api_reports = sp_api_reports
+
+    @retry(
+        attempts=20,
+        delay=30,
+        exceptions=(ReportDocumentNotComplete,),
+    )
+    def get_report(self, report_id: str) -> AmazonReportProduct:
+        data = self.sp_api_reports.get_report(reportId=report_id)
+        logging.info(data.payload)
+        report = AmazonReportProduct(**data.payload)
+        if report.is_complete():
+            if report.is_document_created():
+                return AmazonReportProduct(**data.payload)
+            raise ReportStatusError
+        logging.error('Report not complete \n %s', data.payload)
+        raise ReportDocumentNotComplete
--- src/application/amazon/amazon_report_product_collector/interfaces/amazon_report_creator.py
+++ src/application/amazon/amazon_report_product_collector/interfaces/amazon_report_creator.py
@@ -1,9 +1,19 @@
-from abc import abstractmethod, ABC
-from sp_api.base import Marketplaces, ProcessingStatus, ReportType
+from abc import ABC, abstractmethod
+
+from sp_api.base import ReportType
+
+from src.application.amazon.amazon_report_product_collector.dto.report import AmazonReportProduct
 
 
 class IAmazonReportCreator(ABC):
 
     @abstractmethod
     def create_report(self, report_type: ReportType) -> str:
+        raise NotImplementedError
+
+
+class IAmazonReport(ABC):
+
+    @abstractmethod
+    def get_report(self, report_id: str) -> AmazonReportProduct:
         raise NotImplementedError
